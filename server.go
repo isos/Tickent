bf00dff8700d6d6abfceb6711aee4944ef55ca2e
Clown <tinotomato@hotmail.com>
2017-03-10 14:19:56 +0100

    add http errors

--- server.go
+++ server.go
@@ -17,18 +17,31 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func TpuConnect(w http.ResponseWriter, r *http.Request) {
 	var tpuJson TicketJson
+
+	if r.Body == nil {
+		http.Error(w, "Forbidden", 400)
+		return
+	}
+
 	//rep := r.FormValue("tpu_quKW3zx")
 	for key, _ := range r.Form {
 		err := json.Unmarshal([]byte(key), &tpuJson)
 		if err != nil {
 			fmt.Fprint(w, "Error: ", err)
+			http.Error(w, "Forbidden", 400)
+			return
 		}
 	}
+}
+
+func ClientConn(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
 	fmt.Println("Server Started")
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/tpuconnect", TpuConnect)
+	http.HandleFunc("/client", ClientConn)
 	http.ListenAndServe(":8080", nil)
 }
