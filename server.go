0cd4ffda25aa8c16ac9894088b3ccf12c0dc752d
Clown <tinotomato@hotmail.com>
2017-03-10 13:22:14 +0100

    basic json server handling

--- server.go
+++ server.go
@@ -5,11 +5,23 @@ import (
 	"net/http"
 )
 
+type TicketJson struct {
+	IdU   string
+	items []string
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hola")
 }
 
+func TpuConnect(w http.ResponseWriter, r *http.Request) {
+	var tpuJson TicketJson
+	resp := r.FormValue("tpu_quKW3zx")
+	err = json.Unmarshal(resp, &tpuJson)
+}
+
 func main() {
 	http.HandleFunc("/", Index)
+	http.HandleFunc("/tpuconnect", TpuConnect)
 	http.ListenAndServe(":8080", nil)
 }
