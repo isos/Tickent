8f67fba3637cebaf201604c48bea3948686bd64a
Mester19 <garriga975@gmail.com>
2017-03-11 00:20:03 +0000

    somethingxd

--- server.go
+++ server.go
@@ -36,71 +36,6 @@ type ClientJson struct {
 	Items   []CompraItem
 }
 
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hola")
-}
-
-func TpuConnect(w http.ResponseWriter, r *http.Request) {
-	//get Json from TPU
-	var tpuJson TicketJson
-
-	if r.Body == nil {
-		http.Error(w, "Forbidden", 400)
-		return
-	}
-
-	for key, _ := range r.Form {
-		err := json.Unmarshal([]byte(key), &tpuJson)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			http.Error(w, "Forbidden", 400)
-			return
-		}
-	}
-
-	//put into db
-
-	//open SQL connection
-	db, err := sql.Open("mysql", "ticket:X2L1aLOJ@/tickent")
-	if err != nil {
-		fmt.Println("Error: Failed to open SQL connection")
-		db.Close()
-		return
-	}
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Error: Failed to ping SQL db")
-	}
-
-	//prepared statement from struct values
-	//calculate total first?
-	stmt, err := db.Prepare("INSERT INTO compras(idnfc, idt) VALUES (?, ?)")
-	if err != nil {
-		fmt.Println(err)
-	}
-	_, err := stmt.Exec(tpuJson.idNfc, tpuJson.idTienda)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	//get last idc -> lastId
-
-	var lastId int
-
-	for i := 0; i < len(tpuJson.Items); i++ { //insert all Items in json
-		stmt, err := db.Prepare("INSERT INTO compra(idc, articulo, precio, cantidad) VALUES (?, ?, ?, ?)")
-		if err != nil {
-			fmt.Println(err)
-		}
-		_, err := stmt.Exec(lastId, tpuJson.Items[i].articulo, tpuJson.Items[i].precio, tpuJson.Items[i].cantidad)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-}
-
 func ClientConn(w http.ResponseWriter, r *http.Request) {
   r.ParseForm ()
 
@@ -118,7 +53,9 @@ func ClientConn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  rows, err = db.Query ("SELECT idnfc,model FROM nfc WHERE idu = ?", iduser)
+  query := "SELECT idnfc, model FROM nfc WHERE idu = ?"
+
+  rows, err = db.Query ( query )
   if err != nil {
     http.Error ( w, "Error: Failed executing query", 503 )
     rows.Close ()
@@ -159,14 +96,24 @@ type ClientJson struct {
   */
   var resp ClientJson
 
+  query = "SELECT t.empresa, t.color, t.logo, cs.total, cs.fecha FROM tienda t, compras cs WHERE cs.idnfc = ?"
+
+  for p := 0; p < rows.Next (); p++ {
+    rows, err = db.Query ( query, nfcusers[p] );
+
+    db.Prepare ("SELECT COUNT(idl) FROM compra WHERE idc = ?")
+    db.Exec ( idc )
+
+    for i := 0; 
+
+  resp.Items = make ( []CompraItem, 
+
 	json.NewEncoder(w).Encode(resp)
 }
 
 func main() {
   fmt.Println ("Server Started")
 
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/tpuconnect", TpuConnect)
 	http.HandleFunc("/client", ClientConn)
 
 	http.ListenAndServe(":8080", nil)
