1bdf04671d3b30edb3c651cfb6a9b18d1238aa18
Chris <chrislund97@gmail.com>
2017-03-11 06:57:54 +0100

    Add files via upload
    add QR

--- TPVClient.cs
+++ TPVClient.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Collections.Specialized;
 using System.Net;
-
+using System.Diagnostics;
 
 
 public class ArduinoNFC
@@ -24,6 +24,49 @@ public class ArduinoNFC
     
     public static List<articulo> articulos = new List<articulo>();
     
+    public static void StartQR()
+    {
+        string idc = GetQR();
+        OpenQR(idc);
+    }
+    public static string GetQR()
+    {
+        WebClient webClient = new WebClient();
+        string URL = "http://tickent.tk:8080/tpuconnect";
+        NameValueCollection formData = new NameValueCollection();
+        string id = "borja";
+        string idc = "";
+        
+        formData["json"] =
+            "{\"IdNfc\":\"" +
+             id + 
+            "\",\"IdTienda\":\"1234\",\"Items\":[" + 
+            GetProductoJson()
+            + "]}";
+        
+        byte[] arrByte = webClient.UploadValues(URL, "POST", formData);
+        foreach (byte b in arrByte)
+        {
+            idc += Convert.ToChar(b);
+        }
+        
+        return idc;
+    }
+    public static void OpenQR(string idc)
+    {
+        /*string filename
+            = string.Format(@"{0}\{1}",
+            System.IO.Path.GetTempPath(),
+            "testhtm.htm");
+            
+        File.WriteAllText(filename, "<h1>Hello</h1>");
+        Process.Start(filename);*/
+        
+        //System.Diagnostics.Process.Start("data:image/png;base64," + QR);
+        //Process.Start("data:image/png;base64," + QR);
+        Process.Start("http://Tickent.tk:8080/qr?idc=" + idc);
+    }
+    
     public static void AnadirProductos()
     {
         string inpt = "";
@@ -32,7 +75,12 @@ public class ArduinoNFC
             articulo tempArt;
             Console.Write("Introduce producto: ");
             inpt = Console.ReadLine();
-            if (inpt != "0")
+            
+            if (inpt == "QR" || inpt == "qr" || inpt == "Qr" || inpt == "qR")
+            {
+                StartQR();
+            }
+            else if (inpt != "0")
             {
                 tempArt.nombre = inpt;
                 
@@ -70,8 +118,12 @@ public class ArduinoNFC
              "\",\"Precio\":" + a.precio.ToString() +
               ",\"Cantidad\":" + a.cantidad.ToString() + "},";
         }
-        
-        return prodJson.Substring(0, prodJson.Length -1);
+        if (prodJson.Length < 4)
+        {
+            return "NoProds";
+        }
+        else
+            return prodJson.Substring(0, prodJson.Length -1);
     }
     
     public static string GetID(SerialPort sp)
@@ -95,7 +147,7 @@ public class ArduinoNFC
     {
         string URL = "http://tickent.tk:8080/tpuconnect";
         WebClient webClient = new WebClient();
-
+        
         NameValueCollection formData = new NameValueCollection();
         formData["json"] =
             "{\"IdNfc\":\"" +
@@ -119,12 +171,14 @@ public class ArduinoNFC
         {
             AnadirProductos();
             
+            
             while (id.Length != 32)
             {
                 id = GetID(sp);
                 Thread.Sleep(200);
             }
             SendJson(id);
+            
             id = "";
             
             articulos.Clear();
